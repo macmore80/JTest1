9ce1654ac255eb371c4f94da4bd3e907f01bfb7c
Jongkwan Park <Jongkwan.Park@cognex.com>
2023-02-06 17:45:48 +0900

    Modified code to save csv file after running exe result file

--- JTest1/Program.cs
+++ JTest1/Program.cs
@@ -6,33 +6,36 @@ namespace jkghtest
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Gray;
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
 
-            Console.WriteLine("Step 1. using github");
-
-            Console.WriteLine("Step 2. commit, push, pull");
-
-            Console.WriteLine("Step 2. checking a change after pullin from github to local pc");
-
+            Console.WriteLine("Start JK's Test for using github - 20230206");
+                        
+            //Console.WriteLine("Fill in test log");
             ////Console.ReadKey(); //developer-talk.tistory.com/320
             //Console.WriteLine("Push any key, please");
             //ConsoleKeyInfo keyInfo = Console.ReadKey();
             //Console.WriteLine("What is Key?: " + keyInfo.Key);
 
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"test.csv"))
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"test.csv")) //ssabi.tistory.com/55 - CSV 파일 읽고 쓰기
             {
-                // Define items
-                file.WriteLine("name, grade, class, comparison");
+                if (args.Length == 0)
+                {
+                    file.Write($"This project does not have the application param in debug configuration.");
+                }
+                else if (args.Length > 0)
+                {
+                    // Project's properties >> debug > application parameter : Start Test
+                    file.WriteLine("Exist the application parameters(in debug)");
+                    file.WriteLine("{0}, {1}", args[0], args[1]);
 
-                // fill in values.
-                file.WriteLine("{0},{1},{2},{3}", "JK.Park", "99", "A", "PASS");
-
+                    //// Define items
+                    //file.WriteLine("name, grade, class, comparison");
+                    //// fill in values.
+                    //file.WriteLine("{0},{1},{2},{3}", "JK.Park", "99", "A", "PASS");
+                }
             }
-
-            //ssabi.tistory.com/55 - CSV 파일 읽고 쓰기
-
-
+            Console.WriteLine("End JK's Test for using github - 20230206");
         }
     }
 }
